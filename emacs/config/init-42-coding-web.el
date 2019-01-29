f1b593df307075262c7c25d86f3d1c6c0856503f
Owen Wang <owen.wang@sap.com>
2019-01-29 19:05:38 +0800

    sm

--- emacs/config/init-42-coding-web.el
+++ emacs/config/init-42-coding-web.el
@@ -36,18 +36,48 @@
     (setq web-mode-enable-current-element-highlight t)
     )
   (add-hook 'web-mode-hook  'my-web-mode-hook)
-
-  )
-
-(add-to-list 'auto-mode-alist '("\\.es6\\'" . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-
-(use-package jsx-mode
-  :defer t
-  :init
-  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
-  (autoload 'jsx-mode "jsx-mode" "JSX mode" t)
 )
+
+;---------- tide
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+	:mode (("\\.tsx\\'" . web-mode)
+				("\\.ts\\'" . web-mode))
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+;; formats the buffer before saving
+; (add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+;; TSX
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+;; enable typescript-tslint checker
+; (flycheck-add-mode 'typescript-tslint 'web-mode)
+
+; (use-package jsx-mode
+;   :defer t
+;   :init
+;   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
+;   (autoload 'jsx-mode "jsx-mode" "JSX mode" t)
+; )
 
 ;; == emmet ==
 (use-package emmet-mode
