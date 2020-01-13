6d99343f158174220433afdd01445a115a2d751d
silencej <owen263@gmail.com>
2020-01-13 10:42:02 +0800

    Add emmet mode to markdown mode hook

--- emacs/config/init-42-coding-web.el
+++ emacs/config/init-42-coding-web.el
@@ -51,6 +51,7 @@
   :ensure t
   :defer t
   :init
+	(add-hook 'markdown-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 	(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 	(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 	(add-hook 'web-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
