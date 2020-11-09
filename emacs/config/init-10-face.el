8ec323a6b349a728ffe3478ecb4d6035a6def0a1
silencej <owen263@gmail.com>
2020-11-09 14:44:48 +0800

    Remove ir-black theme, defaults to manoj-dark

--- emacs/config/init-10-face.el
+++ emacs/config/init-10-face.el
@@ -8,9 +8,16 @@
 ;   (setq auto-package-update-hide-results t)
 ;   (auto-package-update-maybe))
 
-(use-package ir-black-theme
-  :config
-  (load-theme 'ir-black t)
+; (use-package ir-black-theme
+;   :config
+;   (load-theme 'ir-black t)
+; )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark)))
 )
 
 (use-package ace-window
